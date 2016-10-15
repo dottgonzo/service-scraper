60a0f056772d7b5798c329aceffff63662fe60b1
Dario Caruso <darioyzf@gmail.com>
2016-10-15 20:35:55 +0200

    up

--- index.ts
+++ index.ts
@@ -1,22 +1,22 @@
 import * as Promise from "bluebird"
 
 interface IScrapeBaseParams {
-uid:string;
-provider:string;
-url:string;
+    uid: string;
+    provider: string;
+    url: string;
 
-img?:string;
+    img?: string;
 
-hqimg?:string;
-mqimg?:string;
-sdimg?:string;
-maximg?:string;
+    hqimg?: string;
+    mqimg?: string;
+    sdimg?: string;
+    maximg?: string;
 }
 
 export default class scrape {
     base_params: IScrapeBaseParams;
     constructor(url: string) {
-        let base_params: IScrapeBaseParams;
+        let base_params=<IScrapeBaseParams>{};
 
         if (url.split('//').length < 2) {
             throw Error('please give a valid url')
@@ -41,16 +41,16 @@ export default class scrape {
                 case "www.youtube.com":
                     uid = query.split('v=')[1].split('&')[0];
 
-                    base_params.uid= uid;
-                        base_params.provider=  'youtube';
-                        base_params.img=  'http://img.youtube.com/vi/' + uid + '/default.jpg'
-                        base_params.hqimg=  'http://img.youtube.com/vi/' + uid + '/hqdefault.jpg'
-                        base_params.mqimg= 'http://img.youtube.com/vi/' + uid + '/mqdefault.jpg'
-                        base_params.sdimg= 'http://img.youtube.com/vi/' + uid + '/sddefault.jpg'
-                        base_params.maximg=  'http://img.youtube.com/vi/' + uid + '/maxresdefault.jpg'
-                        base_params.url= url;
+                    base_params.uid = uid;
+                    base_params.provider = 'youtube';
+                    base_params.img = 'http://img.youtube.com/vi/' + uid + '/default.jpg'
+                    base_params.hqimg = 'http://img.youtube.com/vi/' + uid + '/hqdefault.jpg'
+                    base_params.mqimg = 'http://img.youtube.com/vi/' + uid + '/mqdefault.jpg'
+                    base_params.sdimg = 'http://img.youtube.com/vi/' + uid + '/sddefault.jpg'
+                    base_params.maximg = 'http://img.youtube.com/vi/' + uid + '/maxresdefault.jpg'
+                    base_params.url = url;
 
-                    
+
 
                     break;
 
@@ -59,16 +59,16 @@ export default class scrape {
 
                     uid = query.split('v=')[1].split('&')[0];
 
-                    base_params.uid= uid;
-                        base_params.provider=  'youtube';
-                        base_params.img=  'http://img.youtube.com/vi/' + uid + '/default.jpg'
-                        base_params.hqimg=  'http://img.youtube.com/vi/' + uid + '/hqdefault.jpg'
-                        base_params.mqimg= 'http://img.youtube.com/vi/' + uid + '/mqdefault.jpg'
-                        base_params.sdimg= 'http://img.youtube.com/vi/' + uid + '/sddefault.jpg'
-                        base_params.maximg=  'http://img.youtube.com/vi/' + uid + '/maxresdefault.jpg'
-                        base_params.url= url;
+                    base_params.uid = uid;
+                    base_params.provider = 'youtube';
+                    base_params.img = 'http://img.youtube.com/vi/' + uid + '/default.jpg'
+                    base_params.hqimg = 'http://img.youtube.com/vi/' + uid + '/hqdefault.jpg'
+                    base_params.mqimg = 'http://img.youtube.com/vi/' + uid + '/mqdefault.jpg'
+                    base_params.sdimg = 'http://img.youtube.com/vi/' + uid + '/sddefault.jpg'
+                    base_params.maximg = 'http://img.youtube.com/vi/' + uid + '/maxresdefault.jpg'
+                    base_params.url = url;
 
-                    
+
                     break;
 
                 default:
